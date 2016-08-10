9e162085b9ca09d45473d8ae643b05f7d52455f8
jaamen <jaamen@hotmail.com>
2016-08-10 13:39:43 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,1 +1,10 @@
+var main = function () {
+  $('.menu').click(function() {
+    $('.choices').animate({
+      top: "0px"
+    }, 200);
 
+    $('body').animate({
+      bottom: "285px"
+    }, 200);
+  });
