52e7a6209f34a117c65c259d5250c59775a89b71
jaamen <jaamen@hotmail.com>
2016-08-10 18:37:29 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,10 +1,6 @@
 var main = function () {
-  $('.menu').click(function() {
-    $('.choices').animate({
-      top: "0px"
-    }, 200);
-
-    $('body').animate({
-      bottom: "285px"
-    }, 200);
+  $('.dropdown-toggle').click(function() {
+    $'.dropdown-menu').toggle()
   });
+}
+$(document).ready(main);
