b401b40c7d36347a8a2f0494346d064145133495
jaamen <jaamen@hotmail.com>
2016-08-15 21:51:54 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,8 +1,11 @@
-
-
 var main = function () {
   $('.dropdown-toggle').click(function() {
     $('.dropdown-menu').toggle()
+  });
+
+  $('#arrow-forward').click(function(){
+    var currentSlide = $(.active-slide);
+    var nextSlide = currentSlide.next();
   });
 }
 $(document).ready(main);
