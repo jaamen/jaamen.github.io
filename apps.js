538c2f7ba8fb8c43cbb1b075a922491a42a6c13a
jaamen <jaamen@hotmail.com>
2016-08-28 20:44:10 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -7,24 +7,24 @@ var main = function () {
     var currentSlide = $('.active-slide');
     var nextSlide = currentSlide.next();
   
+   if(nextSlide.length === 0) {
+     nextSlide = $('.slide').first();
+    }
+    
     currentSlide.fadeOut(600).removeClass('active-slide');
     nextSlide.fadeIn(600).addClass('active-slide');
-  
-    if(nextSlide.length === 0) {
-      nextSlide = $('.slide').first();
-    }
   });
 
   $('#arrow-back').click(function() {
     var currentSlide = $('.active-slide');
     var prevSlide = currentSlide.prev();
   
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    prevSlide.fadeIn(600).addClass('active-slide');
-  
-    if(prevSlide.length === 0) {
+     if(prevSlide.length === 0) {
       prevSlide = $('.slide').first();
     }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    prevSlide.fadeIn(600).addClass('active-slide');
   });  
 }  
 $(document).ready(main);
